35c2e1ea53f33b81d03cf1ca04f58ecccb2148e4
Syauqi <syauqi.d.u@gmail.com>
2025-04-18 22:44:23 +0700

    Memecah method getAnnualIncomeTax pada employee dan memecah method calculateTax pada TaxFunction
    Mengurangi beban method yang panjang pada method getAnnualIncomeTax dan TaxFunction dengan memecah fungsional / kerja method  tersebut menjadi beberapa method

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,42 +2,55 @@ package lib;
 
 public class TaxFunction {
 
-	
 	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
+	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus
+	 * dibayarkan setahun.
 	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
+	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan
+	 * bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan)
+	 * dikurangi penghasilan tidak kena pajak.
 	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
+	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena
+	 * pajaknya adalah Rp 54.000.000. Jika pegawai sudah menikah maka penghasilan
+	 * tidak kena pajaknya ditambah sebesar Rp 4.500.000. Jika pegawai sudah
+	 * memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp
+	 * 4.500.000 per anak sampai anak ketiga.
 	 * 
 	 */
-	
-	
-	public static int calculateTax(Person person, Salary salary, int numberOfMonthWorking) {
-		int tax;
-		int numberOfChildren = person.getChildren().size();
+	public static int getNumberOfChildren(Person person) {
+		if (person.getChildren().size() > 3) {
+			return 3;
+		}
+		return person.getChildren().size();
+	}
+
+	public static void checkMonthWorking(int numberOfMonthWorking) {
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (person.getSpouse() != null) {
-			tax = (int) Math.round(0.05 * (((salary.getMonthlySalary() + salary.getOtherMonthlyIncome()) * numberOfMonthWorking) - salary.getAnnualDeductible() - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((salary.getMonthlySalary() + salary.getOtherMonthlyIncome()) * numberOfMonthWorking) - salary.getAnnualDeductible() - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
 	}
-	
+
+	public static int getTidakKenaPajak(Person person, int numberOfChildren) {
+		if (person.getSpouse() != null) {
+			return (54000000 + 4500000 + (numberOfChildren * 1500000));
+		} else {
+			return 54000000;
+		}
+	}
+
+	public static int getTotalIncome(Salary salary) {
+		return salary.getMonthlySalary() + salary.getOtherMonthlyIncome();
+	}
+
+	public static int calculateTax(Person person, Salary salary, int numberOfMonthWorking) {
+		int tax;
+		int numberOfChildren = getNumberOfChildren(person);
+
+		checkMonthWorking(numberOfMonthWorking);
+
+		return (int) Math.round(0.05 * (((getTotalIncome(salary)) * numberOfMonthWorking) - salary.getAnnualDeductible()
+				- getTidakKenaPajak(person, numberOfChildren)));
+
+	}
+
 }
